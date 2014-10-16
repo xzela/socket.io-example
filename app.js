c68ba78055e69951b3ff91f8e694287c0754f658
Zeph Lafassett <zeph@lafassett.com>
2014-10-16 10:53:06 -0700

    adding comments, namespacing log messages

--- app.js
+++ app.js
@@ -2,29 +2,34 @@ var app = require('express')();
 var http = require('http');
 var path = require('path');
 
-var server = require('http').createServer(app).listen(3000, function () {
-	console.log("listening to: ", 3000);
-});
-
-var io = require('socket.io')(server);
-
 app.listen(3001, function () {
-	console.log("listening");
+	console.log('[socket.io]', "creating listner for Socket.io on port:", 3001);
 });
 
-
+// inital route
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// start up the server
+var server = require('http').createServer(app).listen(3000, function () {
+	console.log('[express]', "creating listener for Express on port:", 3000);
+});
+
+var io = require('socket.io')(server);
+
+// connection event
 io.on('connection', function (socket) {
-	console.log('socket:', socket.id, 'has connected');
-	// console.log();
+	// log which socket has connected
+	console.log('[socket.io]', 'socket:', socket.id, 'has connected');
+	// listen for `chatmessage` event
 	socket.on('chatmessage', function (msg) {
-		console.log('socket:', socket.id, 'sez:', msg);
+		console.log('[socket.io]', 'socket:', socket.id, 'sez:', msg);
+		// broadcast 'chatmessage' event to everyone
 		io.emit('chatmessage', msg);
 	});
+	// clean up
 	socket.on('disconnect', function () {
-		console.log('socket:', socket.id, 'has disconnected');
+		console.log('[socket.io]', 'socket:', socket.id, 'has disconnected');
 	})
 });
